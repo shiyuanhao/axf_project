f79cbb1e6da88bf2b9303d5add481e5f00043624
shiyuanhao <493068049@qq.com>
2018-11-02 22:53:22 -0700

    添加闪购超市页面和功能

--- axf/models.py
+++ axf/models.py
@@ -20,3 +20,66 @@ class Nav(Base):
 class Mustbuy(Base):
     class Meta:
         db_table = 'axf_mustbuy'
+
+# insert into axf_shop(img,name,trackid)
+class Shop(Base):
+    class Meta:
+        db_table = 'axf_shop'
+
+# insert into axf_mainshow(trackid,name,img,categoryid,brandname,img1,childcid1,productid1,longname1,price1,marketprice1,img2,childcid2,productid2,longname2,price2,marketprice2,img3,childcid3,productid3,longname3,price3,marketprice3)
+# "21782","优选水果","http://img01.bqstatic.com//upload/activity/2017031018205492.jpg@90Q.jpg","103532","爱鲜蜂","http://img01.bqstatic.com/upload/goods/201/701/1916/20170119164159_996462.jpg@200w_200h_90Q","103533","118824","爱鲜蜂·特小凤西瓜1.5-2.5kg/粒","25.80","25.8","http://img01.bqstatic.com/upload/goods/201/611/1617/20161116173544_219028.jpg@200w_200h_90Q","103534","116950","蜂觅·越南直采红心火龙果350-450g/盒","15.3","15.8","http://img01.bqstatic.com/upload/goods/201/701/1916/20170119164119_550363.jpg@200w_200h_90Q","103533","118826","爱鲜蜂·海南千禧果400-450g/盒","9.9","13.8");
+class Mainshow(models.Model):
+    trackid = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=100)
+    brandname = models.CharField(max_length=100)
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=100)
+    productid1 = models.CharField(max_length=100)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.CharField(max_length=100)
+    marketprice1 = models.CharField(max_length=100)
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=100)
+    productid2 = models.CharField(max_length=100)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.CharField(max_length=100)
+    marketprice2 = models.CharField(max_length=100)
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=100)
+    productid3 = models.CharField(max_length=100)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.CharField(max_length=100)
+    marketprice3 = models.CharField(max_length=100)
+    class Meta:
+        db_table = 'axf_mainshow'
+# insert into axf_foodtypes(typeid,typename,childtypenames,typesort)
+# values("104749","热销榜","全部分类:0",1)
+class Foodtypes(models.Model):
+    typeid = models.CharField(max_length=100)
+    typename = models.CharField(max_length=100)
+    childtypenames = models.CharField(max_length=200)
+    typesort = models.IntegerField()
+    class Meta:
+        db_table = 'axf_foodtypes'
+
+class Goods(models.Model):
+    productid = models.CharField(max_length=10) # 商品ID
+    productimg = models.CharField(max_length=100)   # 商品图片
+    productname = models.CharField(max_length=100)  # 商品名称
+    productlongname = models.CharField(max_length=100)  # 商品弄名称
+    isxf = models.BooleanField(default=False)    # 精选
+    pmdesc = models.BooleanField(default=False)  # 买一送一
+    specifics = models.CharField(max_length=100)   # 规格
+    price = models.DecimalField(max_digits=7, decimal_places=2)    # 价格
+    marketprice = models.DecimalField(max_digits=7, decimal_places=2)  # 商场价格
+    categoryid= models.IntegerField()   # 分类ID
+    childcid = models.IntegerField()    # 子类ID
+    childcidname = models.CharField(max_length=100) # 分类名称
+    dealerid = models.CharField(max_length=10)  # 详情ID
+    storenums = models.IntegerField()   # 库存
+    productnum = models.IntegerField()  # 销量
+
+    class Meta:
+        db_table = 'axf_goods'
--- axf/views.py
+++ axf/views.py
@@ -1,28 +1,99 @@
+import random
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from axf.models import Wheel, Nav, Mustbuy
+from axf.models import Wheel, Nav, Mustbuy, Shop, Foodtypes, Goods, Mainshow
 
 
-def home(request):
+def home(request):  # 首页
+    # 轮播图数据
     wheels = Wheel.objects.all()
+
+    # 导航数据
     navs = Nav.objects.all()
+
+    # 每日必购
     mustbuys = Mustbuy.objects.all()
+
+    # 商品部分
+    shopList = Shop.objects.all()
+    shophead = shopList[0]
+    shoptab = shopList[1:3]
+    shopclass = shopList[3:7]
+    shopcommend = shopList[7:11]
+
+    # 商品主体内容
+    mainshows = Mainshow.objects.all()
+
     data = {
         'wheels':wheels,
         'navs':navs,
-        'mustbuys':mustbuys,
+        'mustbuys': mustbuys,
+        'shophead':shophead,
+        'shoptab':shoptab,
+        'shopclass':shopclass,
+        'shopcommend':shopcommend,
+        'mainshows':mainshows
     }
-    return render(request,'home/home.html',context=data)
+
+    return render(request, 'home/home.html', context=data)
+
+# categoryid 分类ID
+# childid 子类ID
+# sortid 排序ID
+def market(request, categoryid, childid, sortid):    # 闪购超市
+    # 分类信息
+    foodtypes = Foodtypes.objects.all()
+
+    # 分类 点击 下标  >>>>  分类ID
+    typeIndex = int(request.COOKIES.get('typeIndex', 0))
+    # 根据分类下标 获取 对应 分类ID
+    categoryid = foodtypes[typeIndex].typeid
+
+    # 子类信息
+    childtypenames = foodtypes.get(typeid=categoryid).childtypenames
+    # 将每个子类拆分出来
+    childTypleList = []
+    for item in childtypenames.split('#'):
+        arr = item.split(':')
+        dir = {
+            'childname': arr[0],    # 子类名称
+            'childid': arr[1]       # 子类ID
+        }
+        childTypleList.append(dir)
+
+    # 商品信息 - 根据分类id获取对应数据
+    # goodsList = Goods.objects.all()[0:5]
+    if childid == '0':  # 全部分类
+        goodsList = Goods.objects.filter(categoryid=categoryid)
+    else:   # 分类 下 子类
+        goodsList = Goods.objects.filter(categoryid=categoryid, childcid=childid)
 
 
-def market(request):
-    return render(request,'market/market.html')
+    # 排序
+    if sortid == '1':   # 销量排序
+        goodsList = goodsList.order_by('-productnum')
+    elif sortid == '2': # 价格最低
+        goodsList = goodsList.order_by('price')
+    elif sortid == '3': # 价格最高
+        goodsList = goodsList.order_by(('-price'))
+
+    data = {
+        'foodtypes':foodtypes,  # 分类信息
+        'goodsList':goodsList,  # 商品信息
+        'childTypleList': childTypleList,   # 子类信息
+        'categoryid':categoryid,    # 分类ID
+        'childid': childid,     # 子类ID
+    }
+
+    return render(request, 'market/market.html', context=data)
 
 
 def cart(request):
-    return render(request,'cart/cart.html')
+    return render(request, 'cart/cart.html')
 
 
 def mine(request):
-    return render(request,'mine/mine.html')
+    return render(request, 'mine/mine.html')
